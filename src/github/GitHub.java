76f2c7bb49b573e6995c47210d919c31b12f077e
jtm45@students.ptcollege.edu <>
2019-02-01 09:38:16 -0500

    First commit

---
+++ src/github/GitHub.java
@@ -0,0 +1,17 @@
+
+package github;
+
+
+public class GitHub {
+
+    
+    public static void main(String[] args) 
+    {
+       Maul();
+    }
+    public static void Maul()
+    {
+        System.out.println("John Maul!");
+    }
+    
+}
